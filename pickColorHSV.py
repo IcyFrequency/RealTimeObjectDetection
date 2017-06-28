9035e2aefa2ff2827371512fcf135f43017885dc
IcyFrequency <spiners_92@hotmail.com>
2017-06-28 20:53:42 +0200

    Added erosion and dilation

--- pickColorHSV.py
+++ pickColorHSV.py
@@ -4,10 +4,6 @@ import numpy as np
 image_hsv = None   # global ;(
 pixel = (20,60,80) # some stupid default
 
-#red polyp (array([  7, 160, 175], dtype=uint8), array([ -3, 150, 135]), array([ 17, 170, 215]))
-
-#
-
 # mouse callback function
 def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -16,30 +12,41 @@ def pick_color(event,x,y,flags,param):
         #you might want to adjust the ranges(+-10, etc):
         upper =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 40])
         lower =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 40])
-        print(pixel, lower, upper)
+
+        print pixel
+        print lower
+        print upper
 
         image_mask = cv2.inRange(image_hsv,lower,upper)
-        cv2.imshow("mask",image_mask)
+        cv2.imshow("Masking",image_mask)
 
 def main():
     import sys
     global image_hsv, pixel # so we can use it in mouse callback
-
     frame = cv2.imread(sys.argv[1])  # pick.py my.png
     image_src = cv2.resize(frame, (620, 500))
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    erosion = cv2.erode(image_src, kernel, iterations=1)
+    cv2.imshow("Erosion", erosion)
+    dilation = cv2.dilate(erosion, kernel, iterations=1)
+    cv2.imshow("Dilation", dilation )
+
 
     if image_src is None:
         print ("the image read is None............")
         return
-    cv2.imshow("bgr",image_src)
+    cv2.imshow("Original",image_src)
 
     ## NEW ##
-    cv2.namedWindow('hsv')
-    cv2.setMouseCallback('hsv', pick_color)
+    cv2.namedWindow('HSV')
+    cv2.setMouseCallback('HSV', pick_color)
 
     # now click into the hsv img , and look at values:
-    image_hsv = cv2.cvtColor(image_src,cv2.COLOR_BGR2HSV)
-    cv2.imshow("hsv",image_hsv)
+    #image_hsv = cv2.cvtColor(image_src,cv2.COLOR_BGR2HSV)
+    image_hsv = cv2.cvtColor(dilation, cv2.COLOR_BGR2HSV)
+    cv2.imshow("HSV",image_hsv)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
