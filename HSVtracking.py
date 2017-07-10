6978f3534d219af6d84e1c4ff8d378a73221fa46
IcyFrequency <spiners_92@hotmail.com>
2017-07-10 16:12:59 +0200

    Added time measurement

--- HSVtracking.py
+++ HSVtracking.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time
 
+"""
 original = cv2.imread('/Users/pedramsherafat/PycharmProjects/RealTimeObjectDetection/imgs/Adenomatous.jpg')
 frame = cv2.resize(original, (400, 400))
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -26,10 +27,11 @@ cv2.waitKey()
 
 """
 cap = cv2.VideoCapture(0)
-
+startMeasure = time.time()
 while (1):
-
+    startTime = time.time()
     _, original = cap.read()
+
     frame = cv2.resize(original, (400, 400))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -49,10 +51,35 @@ while (1):
     cv2.imshow('Opening', opening)
     cv2.imshow('Closing', closing)
 
-    k = cv2.waitKey(5) & 0xFF
+    elapsedTime = time.time() - startTime
+    elapsedTotTime = (time.time() - startMeasure)
+    #print('function finished in {} ms'.format(float(elapsedTime * 1000)))
+    print('{} \t %f '.format(float(elapsedTime * 1000)) % elapsedTotTime)
+    #rint "time.time(): %f " % time.time()
+
+
+    #print ('FPS: {}'.format(int(1000/(elapsedTime))))
+    k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
 
+TotalTime = time.time() - startMeasure
+print('Total time: {}'.format(float(TotalTime * 1000)))
 cv2.destroyAllWindows()
 cap.release()
+
+""" Read from folder
+
+
+from os import listdir
+from os.path import isfile, join
+import numpy
+import cv2
+
+mypath='/path/to/folder'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+  images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+
 """
